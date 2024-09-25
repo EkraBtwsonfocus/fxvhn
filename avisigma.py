3f09112c78f994ad11b06bf694f9087f852da12e
EkraBtwsonfocus <rtarhanuk@gmail.com>
2024-09-25 21:15:39 +0200

    Add files via upload

---
+++ avisigma.py
@@ -0,0 +1,16 @@
+def func(a, b, c, d): n = (-3 + a*a) * b - 16 / c - 2 * d 
+print(str(n))
+
+print('Введите число A:')
+A = int(input())
+
+print('Введите число B:')
+B = int(input())
+
+print('Введите число C:')
+C = int(input())
+
+print('Введите число D:')
+D = int(input())
+
+func(A, B, C, D)
