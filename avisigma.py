83b28c9b25562e40800d0a549f8287a1c6d60d2d
EkraBtwsonfocus <rtarhanuk@gmail.com>
2024-09-26 11:15:04 +0200

    Update avisigma.py
    Добавил второй вариант, т.к. не знаю какой надо

--- avisigma.py
+++ avisigma.py
@@ -14,3 +14,22 @@ print('Введите число D:')
 D = int(input())
 
 func(A, B, C, D)
+
+
+
+или 
+def foo(a, b, c, d):
+
+2 return((-3+ a2) * b-23) / (c2 * d)
+
+3
+
+4 a= int(input (">>>"))
+
+5 b = int(input (">>>"))
+
+6 c = int(input (">>>"))
+
+7d = int(input (">>>"))
+
+8 print(foo(a, b, c, d))
